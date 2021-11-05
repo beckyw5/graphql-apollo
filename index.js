2bd6840491216ecd4c6fffe0237b5de5b7db2ec7
Becky Wilson <rebecca.wilson2@sky.uk>
2021-11-05 16:54:19 +0000

    example liftcount

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const lifts = require("./data/lifts.json");
 const trails = require("./data/trails.json");
 
 const typeDefs = gql`
+    type Lift {
+        id: ID!
+        name: String!
+        status: String!
+        capacity: Int!
+        night: Boolean!
+        elevationGain: Int!
+    }
     type Query {
-        hello: String!
+        LiftCount: Int!
     }
 `;
 const resolvers = {
     Query: {
-        hello: () => "Hello World!"
+        LiftCount: () => lifts.length
     }
 };
 
